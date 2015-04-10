d95b5b0d99cbd882da1ecf832b27b35bb2f90bda
juanfrc <juanfrc2@gmail.com>
2015-04-10 16:13:06 -0500

    droppable list

--- js/grocery.js
+++ js/grocery.js
@@ -3,3 +3,49 @@
  * What are their properties and methods?
  * How do your objects interact with their respective DOM elements?
  */
+
+// $( "tr.item" ).draggable();
+
+// var item_list ={};
+
+// $(".item").draggable({
+//         helper: "clone",
+//         start: function(event, ui) {
+//             item_list.tr = this;
+//             item_list.helper = ui.helper;
+//         }
+// });
+
+// $("#grocery_list tbody").droppable({
+//     drop: function(event, ui) {
+//         var item = ui.draggable.text();
+//         $(this).find("input").val(item);
+
+//         // $(c.tr).remove();
+//         // $(c.helper).remove();
+//     }
+// });
+$(".item").draggable({
+	helper: "clone",
+	start: function(event, ui){
+		test = this
+	}
+});
+
+$("#grocery_list").droppable({
+	drop: function(){
+		$(test).clone().appendTo("#grocery_list tbody");
+		total();
+	}
+});
+
+var total = function() {
+	var listItems = $("#grocery_list tbody").children().length;
+	for(item = 0; item < listItems; item ++){
+		var test = $("#grocery_list tbody").children()[item];
+		console.log($(test))
+		// price = $(test.children()[1]).html();
+		// console.log(price);
+		// total += price
+	}
+}
