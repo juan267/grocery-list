bfd79f28b814aad059355c16b3da8592eae3e074
juanfrc <juanfrc2@gmail.com>
2015-04-13 13:26:28 -0500

    Add subtotal to grocery list

--- js/grocery.js
+++ js/grocery.js
@@ -4,27 +4,7 @@
  * How do your objects interact with their respective DOM elements?
  */
 
-// $( "tr.item" ).draggable();
-
-// var item_list ={};
-
-// $(".item").draggable({
-//         helper: "clone",
-//         start: function(event, ui) {
-//             item_list.tr = this;
-//             item_list.helper = ui.helper;
-//         }
-// });
-
-// $("#grocery_list tbody").droppable({
-//     drop: function(event, ui) {
-//         var item = ui.draggable.text();
-//         $(this).find("input").val(item);
-
-//         // $(c.tr).remove();
-//         // $(c.helper).remove();
-//     }
-// });
+subtotal = 0
 $(".item").draggable({
 	helper: "clone",
 	start: function(event, ui){
@@ -35,17 +15,13 @@ $(".item").draggable({
 $("#grocery_list").droppable({
 	drop: function(){
 		$(test).clone().appendTo("#grocery_list tbody");
-		total();
+		total(test);
 	}
 });
 
-var total = function() {
-	var listItems = $("#grocery_list tbody").children().length;
-	for(item = 0; item < listItems; item ++){
-		var test = $("#grocery_list tbody").children()[item];
-		console.log($(test))
-		// price = $(test.children()[1]).html();
-		// console.log(price);
-		// total += price
-	}
+var total = function(item) {
+	console.log(item);
+	var price = +$(item).find(".item_price").text();
+	subtotal += price;
+	$("#total_cost").html(subtotal);
 }
