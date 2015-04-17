4e49be29acd420f46caa53f4760486e8d3407a9c
German Hernandez <muygrafico@gmail.com>
2015-04-17 12:03:31 -0500

    basic functionally of drop and count working

--- js/grocery.js
+++ js/grocery.js
@@ -3,3 +3,22 @@
  * What are their properties and methods?
  * How do your objects interact with their respective DOM elements?
  */
+
+
+   $(document).ready(function () {
+
+    $("#store_list tr").draggable({ helper: "clone" });
+    var total_price = 0;
+    $("#grocery_list").droppable({
+
+      drop: function (event, ui) {
+
+        var itemName = ui.draggable.find("td:nth-child(1)").text();
+        var price = ui.draggable.find("td:nth-child(2)").text();
+        total_price = parseFloat(total_price)+parseFloat(price);
+
+        $(this).append('<tr><td>'+ itemName +'</td><td>'+ price +'</td></tr>');
+        $('#total_cost').text(total_price.toFixed(2));
+      }
+    })
+  })
